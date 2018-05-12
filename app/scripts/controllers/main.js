9f4259ac905b76ec4b0df74283bacbfbc4125b1e
Jakub Kawa <kuba.kawa@hotmail.com>
2018-05-12 13:31:43 +0200

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name familiadaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the familiadaApp
+ */
+angular.module('familiadaApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
